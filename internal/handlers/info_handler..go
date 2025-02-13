027b74579dba233ae4cbc3442f05ba09c4a030fb
WhaleShip <andreipogirei@gmail.com>
2025-02-13 22:00:36 +0000

    extract info_service

--- internal/handlers/info_handler..go
+++ internal/handlers/info_handler..go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"log"
 
-	"github.com/whaleship/avito-shop/internal/database/models"
-	"github.com/whaleship/avito-shop/internal/dto"
-	"github.com/whaleship/avito-shop/internal/store"
+	"github.com/whaleship/avito-shop/internal/service"
 	"github.com/whaleship/avito-shop/internal/utils"
 
 	"github.com/gofiber/fiber/v2"
@@ -25,64 +23,10 @@ func InfoHandler(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"errors": "Внутренняя ошибка сервера"})
 	}
 
-	user, err := store.GetUserByUsername(context.Background(), db, username)
+	infoResp, err := service.GetUserInfo(context.Background(), db, username)
 	if err != nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"errors": "Пользователь не найден"})
+		log.Println("Error getting user info: ", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"errors": "Ошибка получения информации"})
 	}
-
-	invItems, err := store.GetInventory(context.Background(), db, user.ID)
-	if err != nil {
-		log.Println("error getting inventory: ", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"errors": "Ошибка при получении инвентаря"})
-	}
-	var inventoryResp []dto.InventoryItemResp
-	for _, item := range invItems {
-		inventoryResp = append(inventoryResp, dto.InventoryItemResp{
-			Type:     item.ItemName,
-			Quantity: item.Quantity,
-		})
-	}
-
-	sentTx, err := store.GetCoinTransactions(context.Background(), db, user.ID, "sent")
-	if err != nil {
-		sentTx = []models.CoinTransaction{}
-	}
-	receivedTx, err := store.GetCoinTransactions(context.Background(), db, user.ID, "received")
-	if err != nil {
-		receivedTx = []models.CoinTransaction{}
-	}
-
-	var sentResp []dto.SentTxResp
-	for _, tx := range sentTx {
-		toUser, err := store.GetUsernameByID(context.Background(), db, tx.ToUserID)
-		if err != nil {
-			toUser = ""
-		}
-		sentResp = append(sentResp, dto.SentTxResp{
-			ToUser: toUser,
-			Amount: tx.Amount,
-		})
-	}
-
-	var received []dto.ReceivedTxResp
-	for _, tx := range receivedTx {
-		fromUser, err := store.GetUsernameByID(context.Background(), db, tx.FromUserID)
-		if err != nil {
-			fromUser = ""
-		}
-		received = append(received, dto.ReceivedTxResp{
-			FromUser: fromUser,
-			Amount:   tx.Amount,
-		})
-	}
-
-	resp := dto.InfoResponse{
-		Coins:     user.Coins,
-		Inventory: inventoryResp,
-		CoinHistory: dto.CoinHistoryResp{
-			Received: received,
-			Sent:     sentResp,
-		},
-	}
-	return c.JSON(resp)
+	return c.JSON(infoResp)
 }
---
+++ internal/service/info_service.go
@@ -0,0 +1,75 @@
+package service
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/whaleship/avito-shop/internal/database/models"
+	"github.com/whaleship/avito-shop/internal/dto"
+	"github.com/whaleship/avito-shop/internal/store"
+)
+
+func GetUserInfo(ctx context.Context, db *pgx.Conn, username string) (*dto.InfoResponse, error) {
+	user, err := store.GetUserByUsername(ctx, db, username)
+	if err != nil {
+		return nil, err
+	}
+
+	invItems, err := store.GetInventory(ctx, db, user.ID)
+	if err != nil {
+		return nil, err
+	}
+	var inventoryResp []dto.InventoryItemResp
+	for _, item := range invItems {
+		inventoryResp = append(inventoryResp, dto.InventoryItemResp{
+			Type:     item.ItemName,
+			Quantity: item.Quantity,
+		})
+	}
+
+	sentTx, err := store.GetCoinTransactions(ctx, db, user.ID, "sent")
+	if err != nil {
+		log.Println("error getting sent transactions: ", err)
+		sentTx = []models.CoinTransaction{}
+	}
+	receivedTx, err := store.GetCoinTransactions(ctx, db, user.ID, "received")
+	if err != nil {
+		log.Println("error getting received transactions: ", err)
+		receivedTx = []models.CoinTransaction{}
+	}
+
+	var sentResp []dto.SentTxResp
+	for _, tx := range sentTx {
+		toUser, err := store.GetUsernameByID(ctx, db, tx.ToUserID)
+		if err != nil {
+			toUser = ""
+		}
+		sentResp = append(sentResp, dto.SentTxResp{
+			ToUser: toUser,
+			Amount: tx.Amount,
+		})
+	}
+
+	var receivedResp []dto.ReceivedTxResp
+	for _, tx := range receivedTx {
+		fromUser, err := store.GetUsernameByID(ctx, db, tx.FromUserID)
+		if err != nil {
+			fromUser = ""
+		}
+		receivedResp = append(receivedResp, dto.ReceivedTxResp{
+			FromUser: fromUser,
+			Amount:   tx.Amount,
+		})
+	}
+
+	infoResp := &dto.InfoResponse{
+		Coins:     user.Coins,
+		Inventory: inventoryResp,
+		CoinHistory: dto.CoinHistoryResp{
+			Received: receivedResp,
+			Sent:     sentResp,
+		},
+	}
+	return infoResp, nil
+}
